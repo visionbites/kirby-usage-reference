34cace0fe60319d89b15071ac59a78453be54ff8
Philip Kuban <kuban@visionbites.de>
2024-02-29 09:46:50 +0100

    extract functionality to a service

---
+++ Services/ReferenceService.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Visionbites\UsageReference\Services;
+
+use Kirby\Toolkit\A;
+
+class ReferenceService
+{
+	public function findReferencesForUuid($uuid, string $template = null)
+	{
+		$cache = kirby()->cache('visionbites.usage-reference');
+		$expire = option('visionbites.usage-reference.expire') <= 1 ? 1 : option('visionbites.usage-reference.expire');
+		$data = $cache->get($uuid);
+
+		if (!$data) {
+
+			if ($template !== null) {
+				$cacheTag = 'index.' . $template;
+
+				$possibleReferences = $cache->get($cacheTag);
+
+				if (!$possibleReferences) {
+					$possibleReferences = kirby()->site()->index(true);
+					$possibleReferences = $possibleReferences->filterBy('intendedTemplate', $template);
+					$possibleReferences = $possibleReferences->toArray();
+
+					$cache->set($cacheTag, $possibleReferences, $expire);
+				}
+			} else {
+				$possibleReferences = $cache->get('index');
+
+				if (!$possibleReferences) {
+					$possibleReferences = kirby()->site()->index(true);
+
+					$possibleReferences = $possibleReferences->toArray();
+
+					$cache->set('index', $possibleReferences, $expire);
+				}
+			}
+
+			$usedItems = A::filter(
+				$possibleReferences,
+				function ($child, $key) use ($uuid) {
+
+					$content = $child['content'];
+					$hasMatch = false;
+
+					foreach ($content as $datum) {
+						// if the content is not pretty printed, the uuid might be masked
+						$maskedUuid = str_replace('/', '\/', $uuid);
+
+						if (str_contains($datum, $uuid) || str_contains($datum, $maskedUuid)) {
+							$hasMatch = true;
+						};
+					}
+					return $hasMatch;
+				}
+			);
+
+			$references = [];
+			foreach ($usedItems as $item) {
+				$refPage = site()->findPageOrDraft($item['uri']);
+
+				$references[] = [
+					'title' => $refPage->title()->value,
+					'url' => $refPage->url(),
+					'slug' => $refPage->slug(),
+					'breadcrumb' => $refPage->panel()->breadcrumb(),
+					'uuid' => $refPage->uuid()->toString(),
+					'template' => $refPage->intendedTemplate()->name(),
+				];
+			}
+			$data = $references;
+
+			$cache->set($uuid, $data, $expire);
+		}
+
+		return $data;
+	}
+
+}
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 use Kirby\Toolkit\I18n;
-use Kirby\Toolkit\A;
+use Visionbites\UsageReference\Services\ReferenceService;
 
 Kirby::plugin('visionbites/usage-reference', [
 	'options' => [
@@ -23,75 +23,11 @@ Kirby::plugin('visionbites/usage-reference', [
 			'computed' => [
 				'references' => function () {
 					$uuid = $this->model()->uuid()->toString();
-					$cache = kirby()->cache('visionbites.usage-reference');
-					$expire = option('visionbites.usage-reference.expire') <= 1 ? 1 : option('visionbites.usage-reference.expire');
-					$data = $cache->get($uuid);
-
-					if (!$data) {
-
-						if ($this->template() !== null) {
-							$cacheTag = 'index.' . $this->template();
-
-							$possibleReferences = $cache->get($cacheTag);
-
-							if(!$possibleReferences) {
-								$possibleReferences = kirby()->site()->index(true);
-								$possibleReferences = $possibleReferences->filterBy('intendedTemplate', $this->template());
-								$possibleReferences = $possibleReferences->toArray();
-
-								$cache->set($cacheTag, $possibleReferences, $expire);
-							}
-						} else {
-							$possibleReferences = $cache->get('index');
-
-							if(!$possibleReferences) {
-								$possibleReferences = kirby()->site()->index(true);
-
-								$possibleReferences = $possibleReferences->toArray();
-
-								$cache->set('index', $possibleReferences, $expire);
-							}
-						}
-
-						$usedItems = A::filter(
-							$possibleReferences,
-							function ($child, $key) use ($uuid) {
-
-								$content = $child['content'];
-								$hasMatch = false;
-
-								foreach ($content as $datum) {
-									// if the content is not pretty printed, the uuid might be masked
-									$maskedUuid = str_replace('/', '\/', $uuid);
-
-									if (str_contains($datum, $uuid) || str_contains($datum, $maskedUuid)) {
-										$hasMatch = true;
-									};
-								}
-								return $hasMatch;
-							}
-						);
-
-						$references = [];
-						foreach ($usedItems as $item) {
-							$refpage = site()->findPageOrDraft($item['uri']);
-
-							$references[] = [
-								'title' => $refpage->title()->value,
-								'url' => $refpage->url(),
-								'breadcrumb' => $refpage->panel()->breadcrumb(),
-								'uuid' => $refpage->uuid()->toString(),
-								'template' => $refpage->intendedTemplate()->name(),
-							];
-						}
-						$data = $references;
-
-						$cache->set($uuid, $data, $expire);
-					}
-
-					return $data;
+					$refService = new ReferenceService();
+					return $refService->findReferencesForUuid($uuid, $this->template());
 				}
 			]
+
 		]
 	]
 ]);
